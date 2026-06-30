e2a406fbc9b6100e3948500dab4f5aba2ce1f5b7
Dhirendra10251 <dhirendra.25bai10251@vitbhopal.ac.in>
2026-06-30 23:21:12 +0530

    Implement longest common prefix function

---
+++ Strings/longest-common-preffix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        if(s.empty()){
+            return "";
+        }
+        string prefix = s[0];
+        for(int i = 1;i<s.size();i++){
+            while(s[i].find(prefix) != 0){
+                prefix.pop_back();
+                if(prefix.empty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+};
